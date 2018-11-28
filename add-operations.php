411ffd690ae187f7d7fcc3871a40d7350cd2dafb
camillenaulet03 <camille.naulet@hotmail.fr>
2018-11-28 16:26:23 +0100

    visuel

---
+++ add-operations.php
@@ -0,0 +1,26 @@
+<?php
+
+?><!doctype html>
+<html lang="an">
+<head>
+    <meta charset="UTF-8">
+    <title>Add operations</title>
+</head>
+<body>
+    <form method="post">
+        <table>
+            <tbody>
+                <tr>
+                    <td><input type="text" name="libellé" placeholder="Libellé" /></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="montant" placeholder="Montant" /></td>
+                </tr>
+                <tr>
+                    <td><button>OK</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
+</body>
+</html>
