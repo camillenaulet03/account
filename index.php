7ca2e9be1ade75eabdbadc0a0a5f3a9d130d4b68
camillenaulet03 <camille.naulet@hotmail.fr>
2018-11-28 16:27:20 +0100

    initialisation

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+//lien de la page d'accueil vers une autre page (opération)
+//opération lien vers la page d'accueil + lien add (ajouter une nouvelle opération (libellé + un + ou -)
+//opération id libellé et montant
+//transaction (libellé et le montant)
