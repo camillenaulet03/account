fbbf6b88ec1ea7f8e862390d371f3044969f1a19
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2018-11-29 14:19:39 +0100

    Add files via upload

---
+++ operations.php
@@ -0,0 +1,59 @@
+<?php
+$host = 'localhost';
+$port = 3306;
+$database = 'test';
+$login = 'root';
+$password = '';
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Libellé</th>
+                <th>Montant</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        try
+        {
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $login, $password);
+        $stmt = $pdo->prepare("SELECT * FROM users");
+        $stmt->execute();
+        while($users = $stmt->fetch()) {
+            //var_dump($users['id']);
+            //var_dump($users['label']);
+            //var_dump($users['montant']);
+            $users = $stmt->fetchObject();
+            $id = $users->id;
+            $label = $users->label;
+            $montant = $users->montant;
+            //var_dump($id);
+        }
+        //$users = $stmt->fetchObject();
+        //$id = $users['id'];
+        //$label = $users['label'];
+        //$montant = $users['montant'];
+        //var_dump($id);
+        ?>
+            <tr>
+                <td><?php echo $id;?></td>
+                <td><?php echo $label;?></td>
+                <td><?php echo $montant;?></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
+<?php }
+catch (PDOException $pdoE)
+	{
+		echo 'PDO Exception: <br/>'.$pdoE->getMessage();
+	}
+	?>
