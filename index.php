bcb29214ffc232950f16b861ec25d0aae2e899f4
ArthurRolland1256 <45195704+ArthurRolland1256@users.noreply.github.com>
2018-11-30 16:07:57 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php
-    session_start();
-?><!DOCTYPE.html>
+<!DOCTYPE.html>
 <html lang="en">
 <head>
     <title>Index</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
     <div align ="center">
         <h1>Accéder Au Opérations</h1>
-        <button><a href="operations.php">Opérations</a></button>
+        <button><a href="operations.php" class="badge badge-white">Opérations</a></button>
     </div>
 </body>
 </html>
