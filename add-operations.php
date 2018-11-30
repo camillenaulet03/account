5889979231e41300809f9ccf8fdcc661205c3268
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2018-11-30 10:08:40 +0100

    Add files via upload

--- add-operations.php
+++ add-operations.php
@@ -1,46 +1,101 @@
-<?php
-//lien de la page d'accueil vers une autre page (opération)
-//opération lien vers la page d'accueil + lien add (ajouter une nouvelle opération (libellé + un + ou -)
-//opération id libellé et montant
-//transaction (libellé et le montant)
+<?php session_start();
+class Transaction {
+    private $label;
+    private $montant;
+
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+    public function getLabel()
+    {
+        return $this->label;
+    }
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+    }
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+}
+
+class Account extends Transaction {
+    private $transactions;
+
+    public function setTransaction($transactions)
+    {
+        $this->transactions = $transactions;
+    }
+    public function getTransactions()
+    {
+        return $this->transactions;
+    }
+    public function addTransaction(Transaction $transaction) {
+        $this->transactions = $transaction;
+    }
+}
+
+class User extends Account {
+    private $accounts;
+
+    public function setAccounts($accounts)
+    {
+        $this->accounts = $accounts;
+    }
+    public function getAccounts()
+    {
+        return $this->accounts;
+    }
+}
+$transaction = new Transaction();
+$account = new Account();
+$account->addTransaction($transaction);
+//var_dump($account);
+
 $host = 'localhost';
 $port = 3306;
 $database = 'test';
 $login = 'root';
 $password = '';
+
 $label=(isset($_POST["label"])) ? $_POST["label"]:false;
 $montant=(isset($_POST["montant"])) ? $_POST["montant"]:false;
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $login, $password);
-    //var_dump($pdo);
-    $stmt = $pdo->prepare('INSERT INTO users (label, montant) VALUES (:label, :montant);');
-    $stmt->bindParam(':label', $label, PDO::PARAM_STR);
-    $stmt->bindParam(':montant', $montant, PDO::PARAM_STR);
-    $stmt->execute();
 
-} catch (PDOException $e) {
-    //var_dump($e->getMessage());
-    var_dump("Vos identifiants de connexion à la base de données ne sont as corrects");
-} finally {
-    $pdo = null;
-}
+    try {
+        if (($label=="") || ($montant=="")) {
+            echo " Merci de remplir tous les champs";
+        } else {
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $login, $password);
+        //var_dump($pdo);
+        $stmt = $pdo->prepare('INSERT INTO users (label, montant) VALUES (:label, :montant);');
+        $stmt->bindParam(':label', $label, PDO::PARAM_STR);
+        $stmt->bindParam(':montant', $montant, PDO::PARAM_STR);
+        $stmt->execute(); }
+        //var_dump($stmt);
+    } catch (PDOException $e) {
+        //var_dump($e->getMessage());
+        var_dump("Vos identifiants de connexion à la base de données ne sont as corrects");
+    } finally {
+        $pdo = null;
+    }
+
 ?><!doctype html>
-<html lang="en">
+<html lang="an">
 <head>
     <meta charset="UTF-8">
     <title>Add operations</title>
-    <link rel="stylesheet" type="text/css" href="hide.css">
 </head>
 <body>
-<form method="post" align="center">
-    <input type="text" name="label" placeholder="Label" value="" /><br>
-    <input type="text" name="montant" placeholder="Montant" value="" /><br>
+<form method="post">
+    <input type="text" name="label" placeholder="Label" value="" />
+    <input type="text" name="montant" placeholder="Montant" value="" />
+    <button type="submit" class="btn btn-primary mb-2">OK</button>
 </form>
-<?php if ($label = ""  && $montant = "") { ?>
-    <button align="center" class="ex1" type="submit">OK</button>
-<?php } else { ?>
-    <button align="center" type="submit">OK</button>
-<?php } ?>
-
+<table>
+    <tbody>
+    </tbody>
+</table>
 </body>
 </html>
